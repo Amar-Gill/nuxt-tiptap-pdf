b549f6353d9580bee7b7f7afbe40623dd3f503f6
Amar Gill <amar.s.gill@outlook.com>
2024-01-17 22:59:17 -0500

    run linter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@vueuse/nuxt"],
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/ui', '@vueuse/nuxt'],
   ui: {
-    icons: ["tabler", "material-symbols"],
+    icons: ['tabler', 'material-symbols'],
   },
 });
