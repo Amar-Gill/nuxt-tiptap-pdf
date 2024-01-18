acf6bd730cc50360f30514115372875b7fd33ae8
Amar Gill <amar.s.gill@outlook.com>
2024-01-17 22:58:57 -0500

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  extends: ['@nuxt/eslint-config'],
+  rules: {
+    // Global
+    quotes: ['error', 'single'],
+    'quote-props': ['error', 'as-needed'],
+    '@typescript-eslint/no-non-null-assertion': 1,
+  },
 };
