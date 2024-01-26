d2c3560aba05f8aaf99ebdb664302b187fa91947
Amar Gill <amar.s.gill@outlook.com>
2024-01-25 23:28:07 -0500

    update editor menu component

--- components/EditorMenu.vue
+++ components/EditorMenu.vue
@@ -1,133 +1,13 @@
 <script lang="ts" setup>
 import type { Editor } from '@tiptap/vue-3';
 
+const { metaSymbol } = useShortcuts();
+
 defineProps<{ editor: Editor }>();
 </script>
 
 <template>
-  <div class="flex justify-between">
-    <span class="flex justify-between gap-10">
-      <span class="flex justify-between gap-1">
-        <UTooltip text="Bold">
-          <UButton
-            square
-            :disabled="!editor.can().chain().focus().toggleBold().run()"
-            :variant="editor.isActive('bold') ? 'outline' : 'ghost'"
-            icon="i-tabler-bold"
-            @click="editor.chain().focus().toggleBold().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Italics">
-          <UButton
-            square
-            :disabled="!editor.can().chain().focus().toggleItalic().run()"
-            :variant="editor.isActive('italic') ? 'outline' : 'ghost'"
-            icon="i-tabler-italic"
-            @click="editor.chain().focus().toggleItalic().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Strikethrough">
-          <UButton
-            square
-            :disabled="!editor.can().chain().focus().toggleStrike().run()"
-            :variant="editor.isActive('strike') ? 'outline' : 'ghost'"
-            icon="i-tabler-strikethrough"
-            @click="editor.chain().focus().toggleStrike().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Code">
-          <UButton
-            square
-            :disabled="!editor.can().chain().focus().toggleCode().run()"
-            :variant="editor.isActive('code') ? 'outline' : 'ghost'"
-            icon="i-tabler-code"
-            @click="editor.chain().focus().toggleCode().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Bullet List">
-          <UButton
-            square
-            :variant="editor.isActive('bulletList') ? 'outline' : 'ghost'"
-            icon="i-tabler-list"
-            @click="editor.chain().focus().toggleBulletList().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Numbered List">
-          <UButton
-            square
-            :variant="editor.isActive('orderedList') ? 'outline' : 'ghost'"
-            icon="i-tabler-list-numbers"
-            @click="editor.chain().focus().toggleOrderedList().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Block Quote">
-          <UButton
-            square
-            :variant="editor.isActive('blockquote') ? 'outline' : 'ghost'"
-            icon="i-tabler-blockquote"
-            @click="editor.chain().focus().toggleBlockquote().run()"
-          />
-        </UTooltip>
-      </span>
-      <span class="flex justify-between gap-1">
-        <UTooltip text="Paragraph">
-          <UButton
-            square
-            :variant="editor.isActive('paragraph') ? 'outline' : 'ghost'"
-            icon="i-material-symbols-format-paragraph-rounded"
-            @click="editor.chain().focus().setParagraph().run()"
-          />
-        </UTooltip>
-        <UTooltip text="Heading 1">
-          <UButton
-            square
-            :variant="
-              editor.isActive('heading', { level: 1 }) ? 'outline' : 'ghost'
-            "
-            icon="i-tabler-h-1"
-            @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-          />
-        </UTooltip>
-        <UTooltip text="Heading 2">
-          <UButton
-            square
-            :variant="
-              editor.isActive('heading', { level: 2 }) ? 'outline' : 'ghost'
-            "
-            icon="i-tabler-h-2"
-            @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-          />
-        </UTooltip>
-        <UTooltip text="Heading 3">
-          <UButton
-            square
-            :variant="
-              editor.isActive('heading', { level: 3 }) ? 'outline' : 'ghost'
-            "
-            icon="i-tabler-h-3"
-            @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-          />
-        </UTooltip>
-        <UTooltip text="Heading 4">
-          <UButton
-            square
-            :variant="
-              editor.isActive('heading', { level: 4 }) ? 'outline' : 'ghost'
-            "
-            icon="i-tabler-h-4"
-            @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-          />
-        </UTooltip>
-        <UTooltip text="Code Block">
-          <UButton
-            square
-            :variant="editor.isActive('codeBlock') ? 'outline' : 'ghost'"
-            icon="i-tabler-terminal-2"
-            @click="editor.chain().focus().toggleCodeBlock().run()"
-          />
-        </UTooltip>
-      </span>
-    </span>
+  <div class="flex justify-between overflow-auto flex-wrap">
     <UButtonGroup>
       <UTooltip text="Clear Formatting">
         <UButton
@@ -145,7 +25,10 @@ defineProps<{ editor: Editor }>();
           @click="editor.chain().focus().setHorizontalRule().run()"
         />
       </UTooltip>
-      <UTooltip text="Hard Break">
+      <UTooltip
+        text="Hard Break"
+        :shortcuts="[metaSymbol, 'Enter']"
+      >
         <UButton
           square
           icon="i-tabler-page-break"
@@ -153,7 +36,10 @@ defineProps<{ editor: Editor }>();
           @click="editor.chain().focus().setHardBreak().run()"
         />
       </UTooltip>
-      <UTooltip text="Undo">
+      <UTooltip
+        text="Undo"
+        :shortcuts="[metaSymbol, 'Z']"
+      >
         <UButton
           square
           :disabled="!editor.can().chain().focus().undo().run()"
@@ -162,7 +48,10 @@ defineProps<{ editor: Editor }>();
           @click="editor.chain().focus().undo().run()"
         />
       </UTooltip>
-      <UTooltip text="Redo">
+      <UTooltip
+        text="Redo"
+        :shortcuts="[metaSymbol, '&#x21E7;', 'Z']"
+      >
         <UButton
           square
           :disabled="!editor.can().chain().focus().redo().run()"
@@ -172,6 +61,161 @@ defineProps<{ editor: Editor }>();
         />
       </UTooltip>
     </UButtonGroup>
+    <span class="flex justify-between gap-10">
+      <span class="flex justify-between gap-1">
+        <UTooltip
+          text="Bold"
+          :shortcuts="[metaSymbol, 'B']"
+        >
+          <UButton
+            square
+            :disabled="!editor.can().chain().focus().toggleBold().run()"
+            :variant="editor.isActive('bold') ? 'outline' : 'ghost'"
+            icon="i-tabler-bold"
+            @click="editor.chain().focus().toggleBold().run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Italics"
+          :shortcuts="[metaSymbol, 'I']"
+        >
+          <UButton
+            square
+            :disabled="!editor.can().chain().focus().toggleItalic().run()"
+            :variant="editor.isActive('italic') ? 'outline' : 'ghost'"
+            icon="i-tabler-italic"
+            @click="editor.chain().focus().toggleItalic().run()"
+          />
+        </UTooltip>
+        <UTooltip text="Strikethrough">
+          <UButton
+            square
+            :disabled="!editor.can().chain().focus().toggleStrike().run()"
+            :variant="editor.isActive('strike') ? 'outline' : 'ghost'"
+            icon="i-tabler-strikethrough"
+            @click="editor.chain().focus().toggleStrike().run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Code"
+          :shortcuts="[metaSymbol, 'E']"
+        >
+          <UButton
+            square
+            :disabled="!editor.can().chain().focus().toggleCode().run()"
+            :variant="editor.isActive('code') ? 'outline' : 'ghost'"
+            icon="i-tabler-code"
+            @click="editor.chain().focus().toggleCode().run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Numbered List"
+          :shortcuts="[metaSymbol, '&#x21E7;', '7']"
+        >
+          <UButton
+            square
+            :variant="editor.isActive('orderedList') ? 'outline' : 'ghost'"
+            icon="i-tabler-list-numbers"
+            @click="editor.chain().focus().toggleOrderedList().run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Bullet List"
+          :shortcuts="[metaSymbol, '&#x21E7;', '8']"
+        >
+          <UButton
+            square
+            :variant="editor.isActive('bulletList') ? 'outline' : 'ghost'"
+            icon="i-tabler-list"
+            @click="editor.chain().focus().toggleBulletList().run()"
+          />
+        </UTooltip>
+        <UTooltip text="Block Quote">
+          <UButton
+            square
+            :variant="editor.isActive('blockquote') ? 'outline' : 'ghost'"
+            icon="i-tabler-blockquote"
+            @click="editor.chain().focus().toggleBlockquote().run()"
+          />
+        </UTooltip>
+      </span>
+      <span class="flex justify-between gap-1">
+        <UTooltip
+          text="Paragraph"
+          :shortcuts="[metaSymbol, '&#9095;', '0']"
+        >
+          <UButton
+            square
+            :variant="editor.isActive('paragraph') ? 'outline' : 'ghost'"
+            icon="i-material-symbols-format-paragraph-rounded"
+            @click="editor.chain().focus().setParagraph().run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Heading 1"
+          :shortcuts="[metaSymbol, '&#9095;', '1']"
+        >
+          <UButton
+            square
+            :variant="
+              editor.isActive('heading', { level: 1 }) ? 'outline' : 'ghost'
+            "
+            icon="i-tabler-h-1"
+            @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Heading 2"
+          :shortcuts="[metaSymbol, '&#9095;', '2']"
+        >
+          <UButton
+            square
+            :variant="
+              editor.isActive('heading', { level: 2 }) ? 'outline' : 'ghost'
+            "
+            icon="i-tabler-h-2"
+            @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Heading 3"
+          :shortcuts="[metaSymbol, '&#9095;', '3']"
+        >
+          <UButton
+            square
+            :variant="
+              editor.isActive('heading', { level: 3 }) ? 'outline' : 'ghost'
+            "
+            icon="i-tabler-h-3"
+            @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Heading 4"
+          :shortcuts="[metaSymbol, '&#9095;', '4']"
+        >
+          <UButton
+            square
+            :variant="
+              editor.isActive('heading', { level: 4 }) ? 'outline' : 'ghost'
+            "
+            icon="i-tabler-h-4"
+            @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+          />
+        </UTooltip>
+        <UTooltip
+          text="Code Block"
+          :shortcuts="[metaSymbol, '&#9095;', 'C']"
+        >
+          <UButton
+            square
+            :variant="editor.isActive('codeBlock') ? 'outline' : 'ghost'"
+            icon="i-tabler-terminal-2"
+            @click="editor.chain().focus().toggleCodeBlock().run()"
+          />
+        </UTooltip>
+      </span>
+    </span>
   </div>
 </template>
 
