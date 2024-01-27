fb43369f8bf57ad4689e31f268be51d1cbc34359
Amar Gill <amar.s.gill@outlook.com>
2024-01-27 11:50:37 -0500

    render content as html with styles

--- pages/render.vue
+++ pages/render.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
+import { generateHTML } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+
 const content = useEditorContent();
+
+const htmlString = generateHTML(content.value!, [StarterKit]);
 </script>
 
 <template>
-  <div>{{ content }}</div>
+  <div
+    class="prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none"
+    v-html="htmlString"
+  />
 </template>
 
 <style scoped></style>
