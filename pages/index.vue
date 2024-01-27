102da177e8e0c5bf28b25a0abb884ee7ca946404
Amar Gill <amar.s.gill@outlook.com>
2024-01-26 22:48:44 -0500

    pdf impl0

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { TipTap } from '#build/components';
+
+const tiptap = ref<InstanceType<typeof TipTap> | null>(null);
+
+const impl0 = async () => {
+  const htmlString = tiptap.value?.editor?.getHTML();
+
+  console.log(htmlString);
+  const div = document.createElement('div');
+
+  const pdf = await htmlToPdf(div, undefined, {
+    html2canvas: { scale: 0.7, useCORS: true },
+  });
+
+  await pdf.html(htmlString!);
+
+  const blob = pdf.output('blob');
+
+  window.open(URL.createObjectURL(blob), '_blank');
+};
+</script>
 
 <template>
   <UPage>
     <UPageHeader title="Home Page" />
     <UPageBody>
       <div>
-        <TipTap :content="{}" />
+        <TipTap
+          ref="tiptap"
+          :content="{}"
+        >
+          <UButton
+            label="impl0"
+            @click="impl0"
+          />
+        </TipTap>
       </div>
     </UPageBody>
   </UPage>
