49b1f496554415bc7c2c95bd25dfbfac6d76cbc8
Amar Gill <amar.s.gill@outlook.com>
2024-01-27 13:00:22 -0500

    pdf impl1

--- pages/render.vue
+++ pages/render.vue
@@ -5,13 +5,21 @@ import StarterKit from '@tiptap/starter-kit';
 const content = useEditorContent();
 
 const htmlString = generateHTML(content.value!, [StarterKit]);
+
+const pdfSection = ref<HTMLElement | null>(null);
 </script>
 
 <template>
-  <div
-    class="prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none"
-    v-html="htmlString"
-  />
+  <div>
+    <UButton @click="exportToPDF('my-pdf-file.pdf', pdfSection)">
+      impl1
+    </UButton>
+    <div
+      ref="pdfSection"
+      class="prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none"
+      v-html="htmlString"
+    />
+  </div>
 </template>
 
 <style scoped></style>
