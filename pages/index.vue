91fdb38a7e91035a18e45db6c013ccb4c39c54c6
Amar Gill <amar.s.gill@outlook.com>
2024-01-31 23:11:20 -0500

    remove import

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import { useHTMLToPDF } from '~/utils/html-to-pdf';
-
 const editorContent = useEditorContent();
 
 const tiptap = ref<InstanceType<typeof TipTap> | null>(null);
