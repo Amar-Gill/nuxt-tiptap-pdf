f5df0011703a0a32fb5ef3d662f76fe5bb421281
Amar Gill <amar.s.gill@outlook.com>
2024-01-31 22:01:30 -0500

    delete older impls

--- pages/index.vue
+++ pages/index.vue
@@ -7,44 +7,6 @@ const tiptap = ref<InstanceType<typeof TipTap> | null>(null);
 
 const pdfGenerating = ref(false);
 
-const impl0 = async () => {
-  const htmlString = tiptap.value?.editor?.getHTML();
-
-  console.log(htmlString);
-  const div = document.createElement('div');
-
-  const pdf = await htmlToPdf(div, undefined, {
-    html2canvas: { scale: 0.7, useCORS: true },
-  });
-
-  await pdf.html(htmlString!);
-
-  const blob = pdf.output('blob');
-
-  window.open(URL.createObjectURL(blob), '_blank');
-};
-
-const impl2 = async () => {
-  pdfGenerating.value = true;
-
-  const htmlString = tiptap.value?.editor?.getHTML();
-
-  await $fetch('/api/pdf', {
-    query: {
-      htmlString,
-    },
-  });
-
-  pdfGenerating.value = false;
-};
-
-const impl3 = async () => {
-  const htmlString = tiptap.value?.editor?.getHTML();
-
-  const x = useHTMLToPDF('abc', htmlString);
-};
-
-// I like this one
 const impl5 = async () => {
   pdfGenerating.value = true;
 
@@ -81,21 +43,6 @@ const impl5 = async () => {
           :delay="300"
           @update="(c) => (editorContent = c)"
         >
-          <UButton
-            label="impl0"
-            @click="impl0"
-          />
-          <UButton
-            label="impl2"
-            :loading="pdfGenerating"
-            @click="impl2"
-          />
-
-          <UButton
-            label="impl3"
-            :loading="pdfGenerating"
-            @click="impl3"
-          />
           <UButton
             label="impl5"
             :loading="pdfGenerating"
