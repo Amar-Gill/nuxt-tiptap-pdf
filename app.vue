92eb6ec3343462526c03086cae6d69f01d035341
Amar Gill <amar.s.gill@outlook.com>
2024-09-20 19:19:26 -0400

    make layout

--- app.vue
+++ app.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts"></script>
 <template>
-  <UHeader>
-    <template #logo>
-      Nuxt Tiptap PDF
-    </template>
-    <template #right>
-      <UColorModeButton />
-    </template>
-  </UHeader>
   <UMain>
-    <UContainer>
+    <NuxtLayout>
       <NuxtPage />
-    </UContainer>
+    </NuxtLayout>
   </UMain>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <UHeader>
+      <template #logo>
+        Nuxt Tiptap PDF
+      </template>
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+    <UContainer>
+      <slot />
+    </UContainer>
+  </div>
+</template>
+
+<style scoped></style>
